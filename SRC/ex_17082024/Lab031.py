538b2570d148647762694002091b3a20adcde05a
RajeshKhaire <rajesh.khaire@gmail.com>
2024-09-06 01:29:43 +0530

    File Added

---
+++ SRC/ex_17082024/Lab031.py
@@ -0,0 +1,30 @@
+# Operator
+
+# Not Operator
+is_pramod_married = True
+print(is_pramod_married)
+print(not is_pramod_married)
+
+# is - this operator java
+
+# Logical Operator -> bool->
+# >, <, >=,<=
+x = 10
+y = 20
+print(x > y)
+print(x < y)
+
+a = 10
+b = 10
+print(a >= b)
+print(a == b)
+print(a <= b)
+
+# OR or And Gate
+f = False
+t = True
+print(f or t)
+print(f and t)
+
+
+
---
+++ SRC/ex_17082024/Lab033.py
@@ -0,0 +1,16 @@
+# Ternary Operator
+
+# If condition then do this
+# else do that
+
+# if My_age > 13 then i can watch reels
+# else i can't watch
+
+# print("I will go Goa" if int(input("Enter your age"))> 18 else "can't go, Stay Home"
+
+user_age = int(input("Enter your age: "))
+
+if user_age > 18:
+    print("I will go Goa")
+else:
+    print("can't go, Stay Home")
